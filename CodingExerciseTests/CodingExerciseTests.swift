660314a4cc4703b9f1c2df6a5dfd8f4000796bfe
Drew Sen <drew@launchtrip.com>
2020-01-16 01:38:06 -0800

    Add unit tests

--- CodingExerciseTests/CodingExerciseTests.swift
+++ CodingExerciseTests/CodingExerciseTests.swift
@@ -9,28 +9,67 @@
 import XCTest
 @testable import CodingExercise
 
+
 class CodingExerciseTests: XCTestCase {
+    
+    var systemUnderTest: AutocompleteViewController!
+    var dataProvider: UserSearchResultDataProvider!
     
     override func setUp() {
         super.setUp()
         // Put setup code here. This method is called before the invocation of each test method in the class.
+        dataProvider = UserSearchResultDataProvider(slackAPI: SlackApi.shared)
+        let viewModel = AutocompleteViewModel(dataProvider: dataProvider)
+        
+         systemUnderTest = AutocompleteViewController(viewModel: viewModel)
+        
+
     }
     
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
+        systemUnderTest = nil
         super.tearDown()
     }
     
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    
+    //Unit test that passes
+    func testSearchBar(){
+        
+        XCTAssert(systemUnderTest.conforms(to: UITextFieldDelegate.self))
+        XCTAssertTrue(self.systemUnderTest.responds(to: (#selector(systemUnderTest.textFieldDidChange(textField:)))))
+        
     }
     
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    
+    //Unit test that fails
+    func testSearchTextField(){
+        XCTAssert(systemUnderTest.conforms(to: UITextFieldDelegate.self))
+        XCTAssertFalse(self.systemUnderTest.responds(to: (#selector(systemUnderTest.textFieldDidChange(textField:)))))
+
+    }
+    
+    //Test case that exercises the UI elements of the view
+    func testTableView(){
+        
+        XCTAssert(systemUnderTest.conforms(to: UITableViewDelegate.self))
+        XCTAssert(systemUnderTest.conforms(to: UITableViewDataSource.self))
+        XCTAssertTrue(self.systemUnderTest.responds(to: (#selector(systemUnderTest.tableView(_:numberOfRowsInSection:)))))
+        XCTAssertTrue(self.systemUnderTest.responds(to: (#selector(systemUnderTest.tableView(_:cellForRowAt:)))))
+        
+    }
+    
+    //end to end testing of json download and mapping
+    func testJSONMapping() {
+        
+        //fetch usernames beginning with t - should be non-empty
+        dataProvider.fetchUsers("t") { users in
+            XCTAssertFalse(users.isEmpty)
         }
+
     }
+
+
     
+
 }
